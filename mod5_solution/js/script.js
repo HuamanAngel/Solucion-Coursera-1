1594a166979bba831a430c5ed9acdb0ef65204fe
HuamanHuamanAngel <oxipusioxx12@gmail.com>
2020-11-25 19:22:04 -0500

    arreglado

--- mod5_solution/js/script.js
+++ mod5_solution/js/script.js
@@ -11,7 +11,6 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
 (function (global) {
   var dc = {};
-
   var homeHtmlUrl = "snippets/home-snippet.html";
   var allCategoriesUrl =
     "https://davids-restaurant.herokuapp.com/categories.json";
@@ -61,55 +60,29 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
   // On page load (before images or CSS)
   document.addEventListener("DOMContentLoaded", function (event) {
-
-    // TODO: STEP 0: Look over the code from
-    // *** start ***
-    // to
-    // *** finish ***
-    // below.
-    // We changed this code to retrieve all categories from the server instead of
-    // simply requesting home HTML snippet. We now also have another function
-    // called buildAndShowHomeHTML that will receive all the categories from the server
-    // and process them: choose random category, retrieve home HTML snippet, insert that
-    // random category into the home HTML snippet, and then insert that snippet into our
-    // main page (index.html).
-    //
-    // TODO: STEP 1: Substitute [...] below with the *value* of the function buildAndShowHomeHTML,
-    // so it can be called when server responds with the categories data.
-
-    // *** start ***
-    // On first load, show home view
     showLoading("#main-content");
     $ajaxUtils.sendGetRequest(
       allCategoriesUrl,
-      buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-      true); // Explicitly setting the flag to get JSON from server processed into an object literal
+      buildAndShowHomeHTML,
+      true);
   });
+
   // *** finish **
 
 
   // Builds HTML for the home page based on categories array
   // returned from the server.
   function buildAndShowHomeHTML(categories) {
-
-    // Load home snippet page
     $ajaxUtils.sendGetRequest(
       homeHtmlUrl,
       function (homeHtml) {
-
-        // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
-        // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
-        // variable's name implies it expects.
-        // var chosenCategoryShortName = ....
-
         var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-        chosenCategoryShortName = " ' " + chosenCategoryShortName + "'";
-
+        chosenCategoryShortName = " ' " + chosenCategoryShortName + " ' ";
 
         var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
         insertHtml("#main-content", homeHtmlToInsertIntoMainPage);
       },
-      false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
+      false);
   }
 
 
